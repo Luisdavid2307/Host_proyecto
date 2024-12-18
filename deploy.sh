492b4df327c361d13f922c122e18ef00239306cf
Luisdavid2307 <luis23david@hotmail.com>
2024-12-17 23:32:26 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 # Asegúrate de que se detengan en caso de error
 set -e
+
 # Instalar Faker si no está instalado
 echo "Instalando Faker..."
 composer require fakerphp/faker --dev
+
 # Instalar dependencias de Composer
 echo "Instalando dependencias de Composer..."
 composer install --no-dev --optimize-autoloader
+
 # Ejecutar migraciones
 echo "Ejecutando migraciones..."
 php artisan migrate --force
-# Ejecutar seeding si es necesario
+
+# Ejecutar seeders si es necesario
 echo "Ejecutando seeders..."
 php artisan db:seed
-# Cachear configuraciones
-echo "Cacheando configuraciones..."
+
+# Borrar cache de configuraciones
+echo "Borrando cache de configuraciones..."
 php artisan config:cache
-# Cachear rutas
-echo "Cacheando rutas..."
+
+# Borrar cache de rua
+echo "Borrando cache de rutas..."
 php artisan route:cache
+
 # Listar rutas para verificar que todo está en orden
 echo "Listando rutas..."
 php artisan route:list
